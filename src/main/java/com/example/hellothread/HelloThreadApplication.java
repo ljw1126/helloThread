038122ae0c4e5cbd893f04aa2640bdadcbb08c8c
leejinwoo <leejinwoo1126@gmail.com>
2024-07-31 20:14:31 +0900

    init

---
+++ src/main/java/com/example/hellothread/HelloThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellothread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloThreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellothread/HelloThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellothread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
