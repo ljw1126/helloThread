080518d43c085b913ba11aab23f6354e5f490dc6
leejinwoo <leejinwoo1126@gmail.com>
2024-07-31 20:43:16 +0900

    section2: 로거 만들기

---
+++ src/main/java/com/example/hellothread/util/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package com.example.hellothread.util;
+
+import static com.example.hellothread.util.MyLogger.*;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("hello thread");
+        log(123);
+    }
+}
