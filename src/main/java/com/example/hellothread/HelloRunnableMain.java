ef5a3f3d61aa5e5fc01e597dbe9ed0712c9b4c91
leejinwoo <leejinwoo1126@gmail.com>
2024-07-31 20:39:06 +0900

    section2: 스레드 생성 - Runnable 인터페이스

---
+++ src/main/java/com/example/hellothread/HelloRunnableMain.java
@@ -0,0 +1,14 @@
+package com.example.hellothread;
+
+public class HelloRunnableMain {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        // 스레드와 해당 스레드가 실행할 작업이 서로 분리되어 있다는 점이 차이
+        HelloRunnable runnable = new HelloRunnable(); // 실행할 작업
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
