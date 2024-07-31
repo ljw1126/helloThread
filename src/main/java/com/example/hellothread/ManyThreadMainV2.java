df47b5d3a7a0fe370ed90cab829b7a10e57c4fb4
leejinwoo <leejinwoo1126@gmail.com>
2024-07-31 20:45:56 +0900

    section2: 여러 스레드 만들기

---
+++ src/main/java/com/example/hellothread/ManyThreadMainV2.java
@@ -0,0 +1,17 @@
+package com.example.hellothread;
+
+import static com.example.hellothread.util.MyLogger.log;
+
+public class ManyThreadMainV2 {
+    public static void main(String[] args) {
+        log("main() start");
+
+        HelloRunnable runnable = new HelloRunnable();
+        for(int i = 1; i <= 100; i++) {
+            Thread thread = new Thread(runnable);
+            thread.start();
+        }
+
+        log("main() end");
+    }
+}
