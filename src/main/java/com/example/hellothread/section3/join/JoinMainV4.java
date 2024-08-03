ddff6b82f557cc70d3ad3f5efcd435c0565e8392
leejinwoo <leejinwoo1126@gmail.com>
2024-08-03 21:04:19 +0900

    section3: join(ms) - 특정 시간만큼만 대기

---
+++ src/main/java/com/example/hellothread/section3/join/JoinMainV4.java
@@ -0,0 +1,62 @@
+package com.example.hellothread.section3.join;
+
+import static com.example.hellothread.util.MyLogger.log;
+import static com.example.hellothread.util.ThreadUtils.sleep;
+
+public class JoinMainV4 {
+    public static void main(String[] args) throws InterruptedException {
+        log("start");
+
+        SumTask task1 = new SumTask(1, 50);
+        SumTask task2 = new SumTask(51, 100);
+
+        Thread thread1 = new Thread(task1, "thread-1");
+        Thread thread2 = new Thread(task2, "thread-2");
+        thread1.start();
+        thread2.start();
+
+        // 스레드가 종료될 때까지 대기
+        log("join(1000) - main 스레드가 thread-* 종료까지 대기");
+        thread1.join(1000);
+        thread2.join(2000);
+        log("join() - main 스레드 대기 완료");
+
+        log("task1.result = " + task1.result);
+        log("task2.result = " + task2.result);
+
+        int sumAll = task1.result + task2.result;
+        log("sum = " + sumAll);
+
+        log("end");
+    }
+
+    static class SumTask implements Runnable {
+        private final int start;
+        private final int end;
+        private int result;
+
+        public SumTask(int start, int end) {
+            this(start, end, 0);
+        }
+
+        public SumTask(int start, int end, int result) {
+            this.start = start;
+            this.end = end;
+            this.result = result;
+        }
+
+        @Override
+        public void run() {
+            log("작업 시작");
+
+            sleep(2000);
+            int sum = 0;
+            for(int i = start; i <= end; i++) {
+                sum += i;
+            }
+
+            result = sum;
+            log("작업 종료 : " + result);
+        }
+    }
+}
