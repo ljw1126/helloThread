1dca8c1224b9740f1a17db1fa91c1409d55906ab
leejinwoo <leejinwoo1126@gmail.com>
2024-08-04 21:29:30 +0900

    section4: 인터럽트 - 시작1

---
+++ src/main/java/com/example/hellothread/section4/ThreadStopMainV1.java
@@ -0,0 +1,31 @@
+package com.example.hellothread.section4;
+
+import static com.example.hellothread.util.MyLogger.log;
+import static com.example.hellothread.util.ThreadUtils.sleep;
+
+public class ThreadStopMainV1 {
+    public static void main(String[] args) {
+        MyTask myTask = new MyTask();
+        Thread thread = new Thread(myTask, "work");
+        thread.start();
+
+        sleep(4000);
+        log("작업 중단 지시 runFlag = false");
+        myTask.runFlag = false;
+    }
+
+    static class MyTask implements Runnable {
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            while(runFlag) {
+                log("작업 중");
+                sleep(3000);
+            }
+
+            log("자원 정리");
+            log("작업 종료");
+        }
+    }
+}
