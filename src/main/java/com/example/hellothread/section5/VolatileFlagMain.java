a8e755d1fc088bdcfeed83b8cbc8a17181e3d556
leejinwoo <leejinwoo1126@gmail.com>
2024-08-05 11:22:17 +0900

    section5: volatile, 메모리 가시성1

---
+++ src/main/java/com/example/hellothread/section5/VolatileFlagMain.java
@@ -0,0 +1,34 @@
+package com.example.hellothread.section5;
+
+import static com.example.hellothread.util.MyLogger.log;
+import static com.example.hellothread.util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+        boolean runFlag = true;
+        //volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while(runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+
+            log("task 종료");
+        }
+    }
+}
